06c3a52f104581e53d0798f33f1325d840cad1ef
dragos.cocos <cocosdragos070788@gmail.com>
2024-01-16 19:44:41 +0200

    add validation for date

--- age_calculator/src/AgeCalculator/DateInput/DateInput.tsx
+++ age_calculator/src/AgeCalculator/DateInput/DateInput.tsx
@@ -1,4 +1,3 @@
-// DateInput.tsx
 import React, { ChangeEvent } from "react";
 
 interface DateInputProps {
@@ -6,19 +5,22 @@ interface DateInputProps {
   name: string;
   value: number | "";
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  errorMessage?: string; // Add this line for the error message
 }
 
-const DateInput = ({ label, name, value, onChange }: DateInputProps) => (
-  <div className="mb-3">
-  <label htmlFor={name} className="form-label">{label}</label>
-  <input
-    type="number"
-    className="form-control"
-    id={name}
-    name={name}
-    value={value}
-    onChange={onChange}
-  />
-</div>
+const DateInput = ({ label, name, value, onChange, errorMessage }: DateInputProps) => (
+  <div className="mb-2">
+    <label htmlFor={name} className="form-label">{label}</label>
+    <input
+      type="number"
+      className={`form-control ${errorMessage ? 'is-invalid' : ''}`} // Add conditional class for error styling
+      id={name}
+      name={name}
+      value={value}
+      onChange={onChange}
+    />
+    {errorMessage && <div className="invalid-feedback">{errorMessage}</div>} {/* Add this line for displaying error */}
+  </div>
 );
+
 export default DateInput;
